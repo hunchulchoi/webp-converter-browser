f906b10ff3b43365e94ad8120142e67e20c569df
hunchulchoi <nettlica@live.co.kr>
2023-10-09 05:50:40 +0900

    1. 비율유지 keepRatio 옵션 추가 2. 입력한 width나 height가 실제 값보다 큰경우 실제 값으로 사용

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,50 @@ interface Options {
   quality?: number
   width?: number
   height?: number
+  keepRatio?: boolean
 }
 
 const DEFAULT_QUALITY = 0.75;
+
+function determineSize(
+  width?: number,
+  originWidth: number,
+  height?: number,
+  originHeight: number,
+  keepRatio?:boolean = false,
+) {
+  // width와 height를 명확히 입력
+  if (width && height) return { width, height };
+
+  if (!width && !height) return { width: originWidth, height: originHeight };
+
+  if (width) {
+    if (width > originWidth) return { width: originWidth, height: originHeight };
+
+    if (!keepRatio) return { width, height: originHeight };
+
+    return { width, height: originHeight / originWidth / width };
+  }
+
+  if (height) {
+    if (height > originHeight) return { width: originWidth, height: originHeight };
+
+    if (!keepRatio) return { width: originWidth, height };
+
+    return { width: originWidth / originHeight / height, height };
+  }
+
+  return { width: originWidth, height: originHeight };
+}
 
 /**
  * When it cause CORS, you may failed to use
  */
 export function srcToWebP(
   src: string,
-  { quality, width, height }: Options = {},
+  {
+    quality, width, height, keepRatio,
+  }: Options = {},
 ): Promise<Blob> {
   return new Promise((resolve, reject) => {
     const canvas = document.createElement('canvas');
@@ -21,8 +55,9 @@ export function srcToWebP(
     image.src = src;
     image.crossOrigin = 'anonymous';
     image.onload = (e) => {
-      canvas.width = width || image.width;
-      canvas.height = height || image.height;
+      const size : {width: number, height: number} = determineSize(width, image.width, height, image.height, keepRatio);
+      canvas.width = size.width;
+      canvas.height = size.height;
       // @ts-ignore
       URL.revokeObjectURL(e.target.src);
       // @ts-ignore
